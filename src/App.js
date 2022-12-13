276e0fb5c3f1f7d6e7b6858083dbc5c382689c21
supradeep <supradeep70@gmail.com>
2022-12-13 16:56:47 +0530

    youtube clone

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Feed,
+  VideoDetail,
+  SearchFeed,
+  Navbar,
+  ChannelDetail,
+} from "./components/index";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/Feed.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { SideBar, Videos } from "./";
+import { fetchFromAPI } from "../utils/FetchFromApi";
+
+function Feed() {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videoss, setVideoss] = useState([]);
+
+  useEffect(() => {
+    setVideoss([]);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideoss(data.items)
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright © 2022 JSM Media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#FC1503" }}>Videos</span>
+        </Typography>
+        <Videos videoss={videoss} />
+      </Box>
+    </Stack>
+  );
+}
+
+export default Feed;
