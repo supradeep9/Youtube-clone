3eb54330423a276d3ac2ad627a016e9981d9745a
supradeep9 <109016263+supradeep9@users.noreply.github.com>
2025-11-30 19:34:03 +0530

    Feed updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -32,7 +32,7 @@ function Feed() {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright © 2022 Supi Media
+          Copyright © 2025 Supi Media
         </Typography>
       </Box>
 
